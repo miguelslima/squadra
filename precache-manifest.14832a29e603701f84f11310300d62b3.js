b3eb3832295ba17cc10340231890547ce3126f70
Miguel <miguelsousalima@hotmail.com>
2020-09-27 10:24:47 -0300

    Updates

---
+++ precache-manifest.14832a29e603701f84f11310300d62b3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "345df84a12243b1082445813e6cc357a",
+    "url": "/squadra/index.html"
+  },
+  {
+    "revision": "1f7e49b7e20c98ef48a1",
+    "url": "/squadra/static/js/2.23fe91d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/squadra/static/js/2.23fe91d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e71a82064f744a2ce048",
+    "url": "/squadra/static/js/main.fee4d446.chunk.js"
+  },
+  {
+    "revision": "f49ba1a54c2c7bd9e593",
+    "url": "/squadra/static/js/runtime-main.87269a2b.js"
+  },
+  {
+    "revision": "32ea00c9ea18efa7f77f550be4c1348f",
+    "url": "/squadra/static/media/404.32ea00c9.gif"
+  },
+  {
+    "revision": "2323d19d1d77ecfab0153e7d8ef16830",
+    "url": "/squadra/static/media/backgroundHome.2323d19d.jpg"
+  },
+  {
+    "revision": "0ab09343f18b549f61d489f93cad9e3b",
+    "url": "/squadra/static/media/button.0ab09343.jpg"
+  },
+  {
+    "revision": "93ad469b5c835cecabc0c8a7ac99c36b",
+    "url": "/squadra/static/media/gokukame.93ad469b.gif"
+  },
+  {
+    "revision": "2c403540507ccf88552b50f18feaca8c",
+    "url": "/squadra/static/media/logo.2c403540.png"
+  },
+  {
+    "revision": "914aef374fe0bb0eea04123f77ff0309",
+    "url": "/squadra/static/media/notFound.914aef37.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.95d1e52edacc3cec56b384caabdbc54f.js"
+  "/squadra/precache-manifest.14832a29e603701f84f11310300d62b3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/squadra/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
